e443526c3a7d555517630e5f91a39cbe4aa6c640
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-12-03 23:29:26 +0300

    DependencyResolver & startup extension added

---
+++ Business/DependencyResolvers/Microsoft/StartupExtensions.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Microsoft
+{
+    public static class StartupExtensions
+    {
+        public static void AddDependencies(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<TodoContext>(opt=>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("Local"));
+            });
+        }
+    }
+}
--- UI/Startup.cs
+++ UI/Startup.cs
@@ -1,3 +1,4 @@
+using Business.DependencyResolvers.Microsoft;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,7 @@ namespace UI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDependencies(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
