32070f76531caf33da621c01e6d6011f4a217857
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-12-04 17:13:31 +0300

    GetAll method added to business layer

---
+++ Business/Concrete/WorkManager.cs
@@ -0,0 +1,41 @@
+﻿using Business.Interface;
+using DataAccess.UnitOfWork;
+using Dtos.WorkDtos;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class WorkManager : IWorkService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public WorkManager(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<List<WorkListDto>> GetAll()
+        {
+            var list = await _unitOfWork.GetRepository<Work>().GetAll();
+            var workList = new List<WorkListDto>();
+            if (list != null & list.Count > 0)
+            {
+                foreach (var work in list)
+                {
+                    workList.Add(new()
+                    {
+                        Definition = work.Definition,
+                        IsCompleted = work.IsCompleted,
+                        Id = work.Id
+                    });
+                }
+            }
+            return workList;
+        }
+    }
+}
---
+++ Business/Interface/IWorkService.cs
@@ -0,0 +1,14 @@
+﻿using Dtos.WorkDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interface
+{
+    public interface IWorkService
+    {
+        Task<List<WorkListDto>> GetAll();
+    }
+}
