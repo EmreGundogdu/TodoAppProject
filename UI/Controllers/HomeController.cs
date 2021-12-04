07b5d4760c2216b58103433de31c3540a1e6950a
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-12-04 17:40:02 +0300

    Mvc added & started

---
+++ UI/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Business.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWorkService _workService;
+
+        public HomeController(IWorkService workService)
+        {
+            _workService = workService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var workList = await _workService.GetAll();
+            return View(workList);
+        }
+    }
+}
