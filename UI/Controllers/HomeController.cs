c7a676263ad2221a029a0290a40b1fbb7ba2304e
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-12-05 19:16:26 +0300

    remove method updated & post update method

--- UI/Controllers/HomeController.cs
+++ UI/Controllers/HomeController.cs
@@ -41,5 +41,15 @@ namespace UI.Controllers
             var dto = await _workService.GetById(id);
             return View(dto);
         }
+        [HttpPost]
+        public async Task<IActionResult> Update(WorkUpdateDto dto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _workService.Update(dto);
+                return RedirectToAction("Index");
+            }
+            return View(dto);
+        }
     }
 }
